53a447be8091d74d130194ca69925e0626b1a387
Jefferson Ferreira <jefferson14489@gmail.com>
2024-06-06 16:44:55 -0300

    :sparkles: Validando token

--- src/main/java/br/com/jeffersonferreira/gestao_vagas/modules/company/use_cases/CreateCompanyUseCase.java
+++ src/main/java/br/com/jeffersonferreira/gestao_vagas/modules/company/use_cases/CreateCompanyUseCase.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import br.com.jeffersonferreira.gestao_vagas.exceptions.UserFoundException;
 import br.com.jeffersonferreira.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.jeffersonferreira.gestao_vagas.modules.company.repositories.CompanyRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class CreateCompanyUseCase {
@@ -13,12 +14,19 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity) {
 
         this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+
+        companyEntity.setPassword(password);
 
         return this.companyRepository.save(companyEntity);
     }
