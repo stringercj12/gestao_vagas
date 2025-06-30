db98010d6164c88251cf86154abd485d335ad39c
Jefferson Ferreira <jefferson.ferreira@mjv.com.br>
2025-06-30 15:34:06 -0300

    wip

---
+++ src/main/java/jcf/gestao_vagas/exceptions/ErrorMessageDto.java
@@ -0,0 +1,11 @@
+package jcf.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDto {
+  private String message;
+  private String field;
+}
---
+++ src/test/java/jcf/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package jcf.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
